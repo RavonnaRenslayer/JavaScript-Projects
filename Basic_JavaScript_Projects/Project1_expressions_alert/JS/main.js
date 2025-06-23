8037a40217ceaa47a9bdf3f18dc1db9444fcd198
Juliana Rocha <julianarocha@outlook.ie>
2025-06-23 13:50:25 +0100

    Added course chanllenges to the js file

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+var message = "Hello from JavaScript!";
+
+//Setting an alert
+window.alert(message);
+
+// Using additional qotations
+document.write("With JavaScript when you need to use additional quotation marks the solution is to add a backslash \\ this is referred to as \"escaping\".<br><br>")
+
+//Concatenating a string
+document.write(" Concatenating a string is when you take a piece of data and stick it on the end of another piece of data."
+    + " To concatenate a string in JavaScript, you use the + operator.<br><br>")
+
+//Assigning a concatenated string to a variable
+var message = "Javascript makes web pages " + "interactive and dynamic.<br><br>";
+
+// Displaying it in the browse
+document.write(message);
+
+//Assigning multiple variables
+var Family = "Rocha\'s", Dad = "DJ", Mom = "JJ", Daughter = "Scar";
+document.write(Dad + "<br><br>");
+
+//Writing an expression
+document.writeln(Dad + " is the dad<br>");
