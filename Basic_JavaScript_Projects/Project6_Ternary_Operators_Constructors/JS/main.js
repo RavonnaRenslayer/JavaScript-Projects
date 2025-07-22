498f09a58449961bfb85fe24ad905c5c4ca133a4
Juliana Rocha <julianarocha@outlook.ie>
2025-07-22 11:01:31 +0100

    Added Ternary_Operators_Vote_Program folder

--- Basic_JavaScript_Projects/Project6_Ternary_Operators_Constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_Ternary_Operators_Constructors/JS/main.js
@@ -1,6 +1,6 @@
 function Ride_Function() {
-	var Height, Can_ride;
-	Height = document.getElementById("Height").value;
-	Can_ride = (Heith < 52) ? "You are too short" : "You  are tall enough.";
+	var Height = parseInt(document.getElementById("Height").value);
+    
+    var Can_ride = (Height < 52) ? "You are too short" : "You  are tall enough";
 	document.getElementById("Ride").innerHTML= Can_ride + " to ride.";
 }
