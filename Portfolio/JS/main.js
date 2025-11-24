d04ffdbe72e8fe3298fcbcdc2ec724557fa7b286
Juliana Rocha <julianarocha@outlook.ie>
2025-11-24 09:30:38 +0000

    Edited Github, Contact and video section. Made changes to the html and css files

--- Portfolio/JS/main.js
+++ Portfolio/JS/main.js
@@ -20,18 +20,25 @@ function currentSlide (n) {
 }
 
 function showSlides (n) {
-    var slides = document.getElementById("mySlides");
-    var dots = document.getElementById("dot");
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
 
-    if (n > slides.length) {slideIndex = 1};
-    if (n < 1) {slideIndex = slides.length};
-    for (i = 0; i < slides.length; i++) {
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     } 
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className += " active ";
-        image
+    slides[slideIndex-1].style.display = "block";
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    if(dots.length > 0) {
+        dots[slideIndex - 1].className += " active";
     }
 }
 
